d7110d9f7b11636bef548109a9564acdbc03f197
Girts Silis <girts.silis@gmail.com>
2017-06-19 11:39:21 -0500

    Barebones webpack/react

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var APP_DIR = path.resolve(__dirname, 'src/');
+var DIST_DIR = path.resolve(__dirname, 'dist/app/');
+
+module.exports = {
+  entry: APP_DIR + '/main.js',
+  output: {
+    path: DIST_DIR,
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules',
+        query: {
+          presets: [
+            'es2016',
+            'react'
+          ]
+        }
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'src/components')
+    ],
+    extensions: ['.js', '.json', '.jsx'],
+  }
+};
